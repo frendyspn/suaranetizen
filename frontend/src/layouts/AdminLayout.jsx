fad13855114cf7ea25f1329bc53c4730a0d3d135
frendy <frendyspn@gmail.com>
2025-07-19 03:38:24 +0700

    thumbnail

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -1,14 +1,23 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import Sidebar from '../components/Sidebar';
 import Navbar from '../components/NavbarAdmin';
 import Footer from '../components/Footer';
+import { Helmet } from 'react-helmet';
+import { SettingContext } from '../contexts/SettingContext';
+import { API_BASE_URL } from '../constants';
 
 const AdminLayout = ({ children }) => {
+    const settings = useContext(SettingContext);
     const [sidebarOpen, setSidebarOpen] = useState(false);
     const toggleSidebar = () => setSidebarOpen(prev => !prev);
 
     return (
         <div >
+            <Helmet>
+                {settings.site_thumbnail && (
+                    <meta property="og:image" content={`${API_BASE_URL}uploads/${settings.site_thumbnail}`} />
+                )}
+            </Helmet>
             <div className="side-overlay"></div>
             <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar}/>
             <div className="dashboard-main-wrapper">
--- frontend/src/layouts/UserLayout.jsx
+++ frontend/src/layouts/UserLayout.jsx
@@ -1,13 +1,21 @@
-import React, {} from 'react';
+import React, {useContext} from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { Helmet } from 'react-helmet';
+import { SettingContext } from '../contexts/SettingContext';
+import { API_BASE_URL } from '../constants';
 
 const UserLayout = ({ children }) => {
-    
+    const settings = useContext(SettingContext);
     
 
     return (
         <div >
+            <Helmet>
+                            {settings.site_thumbnail && (
+                                <meta property="og:image" content={`${API_BASE_URL}uploads/${settings.site_thumbnail}`} />
+                            )}
+                        </Helmet>
             {/* <div className="side-overlay"></div> */}
             {/* <SidebarUser isOpen={sidebarOpen} toggleSidebar={toggleSidebar}/> */}
             <div className="dashboard-main bg-white">
