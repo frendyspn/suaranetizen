86b412c213c256f725de79acc5350233039ff6bd
frendy <frendyspn@gmail.com>
2025-08-18 23:29:37 +0700

    change api url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,7 @@
 // export const API_BASE_URL = "https://apitrial.memoliworks.com/";
 // export const API_BASE_URL = "http://172.20.41.64:8000/"; // Replace with your actual API base URL
-export const API_BASE_URL = "http://192.168.100.18:8000/"; // Replace with your actual API base URL
-// export const API_BASE_URL = "https://suaranetizen.co.id/api/";
+// export const API_BASE_URL = "http://192.168.100.18:8000/"; // Replace with your actual API base URL
+export const API_BASE_URL = "https://suaranetizen.co.id/api/";
 
 export const STORAGE_URL = `${API_BASE_URL}storage/`;
 
